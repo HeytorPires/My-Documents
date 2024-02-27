68755c72857e5b491868d0b2ae456148790bef56
HeytorPires <heytor.cacho29@gmail.com>
2024-02-27 10:38:59 -0300

    Implementando Input text na tela de criar documento com mui

--- frontend/src/forms.jsx
+++ frontend/src/forms.jsx
@@ -3,6 +3,8 @@ import { api } from './lib/axios'
 import { useNavigate } from 'react-router-dom'
 import Button from '@mui/material/Button'
 import Stack from '@mui/material/Stack'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function Forms() {
   const [title, setTitle] = useState()
@@ -28,18 +30,33 @@ function Forms() {
       <h1>Criar Novo Documento</h1>
       <form action="" onSubmit={(event) => createDocument(event)}>
         <h3>Titulo</h3>
-        <input
-          type="text"
-          placeholder="Digite um Titulo"
-          onBlur={(e) => setTitle(e.target.value)}
-        />
+         <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField 
+      id="outlined-basic" 
+      label="Titulo" 
+      variant="outlined" 
+      type="text"
+      placeholder="Digite um Titulo"
+      onBlur={(e) => setTitle(e.target.value)}/>
+   
         <br />
         <h3>Autor</h3>
-        <input
-          type="text"
-          placeholder="Digite o Autor"
-          onBlur={(e) => setAuthor(e.target.value)}
-        />
+        <TextField 
+        id="outlined-basic" 
+        label="Autor" 
+        variant="outlined" 
+        type="text"
+        placeholder="Digite o autor"
+        onBlur={(e) => setAuthor(e.target.value)}/>
+         </Box>
+        
         <br />
         <br />
         <Stack spacing={2} direction="row">
